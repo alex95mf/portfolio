96037f5792cebb08c8316295899eaee04b309eaf
Alexander Martinez <alex95mf@gmail.com>
2023-08-04 17:40:10 -0500

    UPD: Refactorizacion navegacion

--- portfolio/src/components/Navigation.jsx
+++ portfolio/src/components/Navigation.jsx
@@ -3,58 +3,40 @@ import '../styles/navigation.css'
 
 function Navigation() {
     const pagina = {
-        inicio: "inicio",
-        habilidades: "habilidades",
-        proyectos: "proyectos",
-        sobreMi: "sobreMi",
-        contacto: "contacto"
-    }
-
-    const [paginaSeleccionada, setPaginaSeleccionada] = useState(pagina.inicio)
-
-    // Funcion para sombrear la pagina activa
-    const handleInicio = () => {
-        setPaginaSeleccionada(pagina.inicio);
-    };
-    const handleHabilidades = () => {
-        setPaginaSeleccionada(pagina.habilidades);
-    };
-    const handleProyectos = () => {
-        setPaginaSeleccionada(pagina.proyectos);
-    };
-    const handleSobreMi = () => {
-        setPaginaSeleccionada(pagina.sobreMi);
-    };
-    const handleContacto = () => {
-        setPaginaSeleccionada(pagina.contacto);
+        inicio: 'Inicio',
+        habilidades: 'Habilidades',
+        proyectos: 'Proyectos',
+        sobreMi: 'Sobre mi',
+        contacto: 'Contacto',
     };
 
+    const [paginaSeleccionada, setPaginaSeleccionada] = useState(pagina.inicio);
+
+    const handlePaginaClick = (nombrePagina) => {
+        setPaginaSeleccionada(nombrePagina);
+    };
 
     return (
         <>
             <div className='navigation-box'>
                 <nav className='navigation'>
                     <ul>
-                        <li><a className={`nav-item ${paginaSeleccionada == pagina.inicio
-                            ? 'active' : ''}`} onClick={handleInicio}
-                            href='#'>Inicio</a></li>
-                        <li><a className={`nav-item ${paginaSeleccionada == pagina.habilidades
-                            ? 'active' : ''}`} onClick={handleHabilidades}
-                            href='#'>Habilidades</a></li>
-                        <li><a className={`nav-item ${paginaSeleccionada == pagina.proyectos
-                            ? 'active' : ''}`} onClick={handleProyectos}
-                            href='#'>Proyectos</a></li>
-                        <li><a className={`nav-item ${paginaSeleccionada == pagina.sobreMi
-                            ? 'active' : ''}`} onClick={handleSobreMi}
-                            href='#'>Sobre mi</a></li>
-                        <li><a className={`nav-item ${paginaSeleccionada == pagina.contacto
-                            ? 'active' : ''}`} onClick={handleContacto}
-                            href='#'>Contacto</a></li>
+                        {Object.values(pagina).map((nombrePagina) => (
+                            <li key={nombrePagina}>
+                                <a
+                                    className={`nav-item ${paginaSeleccionada === nombrePagina ? 'active' : ''}`}
+                                    onClick={() => handlePaginaClick(nombrePagina)}
+                                    href='#'
+                                >
+                                    {nombrePagina}
+                                </a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
             </div>
         </>
-    )
+    );
 }
 
 export default Navigation
